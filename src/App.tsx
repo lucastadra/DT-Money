c1134d55c827a5c725e2b30ed378ae3ec7321769
Lucas Tadra <lucastadra16@gmail.com>
2022-11-30 22:50:40 -0300

    Definição de estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from './styles/global';
+
 export function App() {
-  return (
-    <div className="App">
-      <h1>Hello WOrld</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello World</h1>
+            <GlobalStyle />
+        </div>
+    );
 }
