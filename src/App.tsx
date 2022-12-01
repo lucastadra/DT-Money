bcf59e302b8bc673cd1a697bcc4663544923a38f
Lucas Tadra <lucastadra16@gmail.com>
2022-12-01 01:38:18 -0300

    Componente NewTransactionModal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 import { GlobalStyle } from './styles/global';
+import { useState } from 'react';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 export function App() {
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+        useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
+
     return (
         <>
-            <Header />
+            <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
             <Dashboard />
+            <NewTransactionModal
+                isOpen={isNewTransactionModalOpen}
+                onRequestClose={handleCloseNewTransactionModal}
+            />
             <GlobalStyle />
         </>
     );
